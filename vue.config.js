7bc5d94d258983594b994fe11fbc2a791247583a
ncdhz <1137436221@qq.com>
2020-08-07 18:01:36 +0800

    README INIT

---
+++ vue.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                "electronDownload": {
+                    "mirror": "https://npm.taobao.org/mirrors/electron/"
+                },
+                "appId": "com.github.ncdhz.privacy",
+                "productName": "innermost",
+                "copyright": "Copyright © 2020 ncdhz",
+                "directories": {
+                    "output": "./dist_electron"
+                }, 
+                "dmg": {
+                    "contents": [
+                        {
+                            "x": 410,
+                            "y": 150,
+                            "type": "link",
+                            "path": "/Applications"
+                        },
+                        {
+                            "x": 130,
+                            "y": 150,
+                            "type": "file"
+                        }
+                    ]
+                },
+                "mac": {
+                    "icon": "./public/icons/icon.icns"
+                },
+                "win": {
+                    "icon": "./public/icons/icon.ico"
+                },
+                "linux": {
+                    "icon": "./public/icons"
+                }
+            }
+        }
+    }
+}
