e94f92cc7ed88d5547dcc3a2b9f38aac77001979
ncdhz <1137436221@qq.com>
2020-08-13 00:27:07 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,42 @@
 module.exports = {
-    pluginOptions: {
-        electronBuilder: {
-            builderOptions: {
-                "electronDownload": {
-                    "mirror": "https://npm.taobao.org/mirrors/electron/"
-                },
-                "appId": "com.github.ncdhz.innermost",
-                "productName": "innermost",
-                "copyright": "Copyright © 2020 ncdhz",
-                "directories": {
-                    "output": "./dist_electron"
-                }, 
-                "dmg": {
-                    "contents": [
-                        {
-                            "x": 410,
-                            "y": 150,
-                            "type": "link",
-                            "path": "/Applications"
-                        },
-                        {
-                            "x": 130,
-                            "y": 150,
-                            "type": "file"
-                        }
-                    ]
-                },
-                "mac": {
-                    "icon": "./public/icons/icon.icns"
-                },
-                "win": {
-                    "icon": "./public/icons/icon.ico"
-                },
-                "linux": {
-                    "icon": "./public/icons"
-                }
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        electronDownload: {
+          mirror: 'https://npm.taobao.org/mirrors/electron/'
+        },
+        appId: 'com.github.ncdhz.innermost',
+        productName: 'innermost',
+        copyright: 'Copyright © 2020 ncdhz',
+        directories: {
+          output: './dist_electron'
+        },
+        dmg: {
+          contents: [
+            {
+              x: 410,
+              y: 150,
+              type: 'link',
+              path: '/Applications'
+            },
+            {
+              x: 130,
+              y: 150,
+              type: 'file'
             }
+          ]
+        },
+        mac: {
+          icon: './public/icons/icon.icns'
+        },
+        win: {
+          icon: './public/icons/icon.ico'
+        },
+        linux: {
+          icon: './public/icons'
         }
+      }
     }
+  }
 }
